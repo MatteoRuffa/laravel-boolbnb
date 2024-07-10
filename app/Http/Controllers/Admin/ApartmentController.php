6cda8fb9b6cc659c579bc75edd415df5d0e3a3f7
MatteoRuffa <cortese_17@protonmail.com>
2024-07-10 17:19:45 +0200

    some add

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -142,7 +142,7 @@ class ApartmentController extends Controller
                 'verify' => false,
             ]);
             $apiBaseUrl='https://api.tomtom.com/search/2/geocode/';
-            $apiAdress= Apartment::apiFormatAddress($validatedData['address']);
+            $apiAdress= Search::apiFormatAddress($validatedData['address']);
             $response = $client->get( $apiBaseUrl . $apiAdress . '.json', [
                 'query' => [
                     'key' => env('TOMTOM_API_KEY'),
