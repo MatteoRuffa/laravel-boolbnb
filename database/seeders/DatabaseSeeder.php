4739d6e2b138ae8f34bfbfdb0f68033e11de1e3e
MatteoRuffa <cortese_17@protonmail.com>
2024-07-12 11:45:11 +0200

    fix_seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ApartmentsSeeder::class,
             PromotionSeeder::class,
+            ServiceSeeder::class,
         ]);
     }
 }
